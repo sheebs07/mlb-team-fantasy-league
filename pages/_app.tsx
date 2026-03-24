1580e7b36aefe09ca083af265d961bca8ddba926
sheebs07 <brianshebik@gmail.com>
2026-03-23 20:23:00 -0500

    adding dark bar user interface

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,15 @@ import "../app/globals.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
-        <Link href="/" style={{ marginRight: "15px" }}>Home</Link>
-        <Link href="/draft" style={{ marginRight: "15px" }}>Draft</Link>
-        <Link href="/standings" style={{ marginRight: "15px" }}>Standings</Link>
-        <Link href="/owners" style={{ marginRight: "15px" }}>Owners</Link>
+      <nav className="navbar">
+        <Link href="/">Home</Link>
+        <Link href="/draft">Draft</Link>
+        <Link href="/standings">Standings</Link>
+        <Link href="/owners">Owners</Link>
         <Link href="/teams">Teams</Link>
       </nav>
 
-      <main style={{ padding: "20px" }}>
+      <main>
         <Component {...pageProps} />
       </main>
     </div>
